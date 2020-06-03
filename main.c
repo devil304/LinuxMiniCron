29c95a4c1b535ccbc5269eab3aa0595774b6542a
Szymon Rzosinski <szymonrzosinski@gmail.com>
2020-06-03 21:56:10 +0200

    asdadwwd

--- main.c
+++ main.c
@@ -8,6 +8,8 @@
 #include <syslog.h>
 #include <string.h>
 #include <syslog.h>
+#include <time.h>
+#include <signal.h>
 
 FILE *task_file;
 //FILE *out_file;
@@ -19,27 +21,75 @@ struct task
     char *command;
     int info;
 };
-struct task tasks[10];
+struct task *tasks;
+int swap(struct task *task1, struct task *task2)
+{
+    struct task taskTmp = *task1;
+    *task1 = *task2;
+    *task2 = taskTmp;
+    return 0;
+}
+struct tm *info;
+int count = 0;
+time_t current_time;
+void AlarmHandler(int sig)
+{
+    signal(SIGALRM, SIG_IGN);
+    pid_t pidF;
+    pidF = fork();
+    if (pidF < 0)
+    {
+        exit(EXIT_FAILURE);
+    }
+    else if (pidF == 0)
+    {
+        execl(tasks[count].command, NULL);
+    }
+    count++;
+    current_time = time(NULL);
+    info = localtime(&current_time);
+    info->tm_hour = tasks[count].h;
+    info->tm_min = tasks[count].m;
+    info->tm_sec = 0;
+    alarm(difftime(timelocal(info), current_time));
+    signal(SIGALRM, AlarmHandler);
+}
+int test(){
+    pid_t pidF;
+    pidF = fork();
+    if (pidF < 0)
+    {
+        exit(EXIT_FAILURE);
+    }
+    else if (pidF == 0)
+    {
+        execl("/mnt/f/OS/Proj/Dem", NULL);
+    }
+    return 0;
+}
 int main(int argc, char *argv[])
 {
-    printf("S1");
-    if (argc == 3)
+    //printf("S1");
+    if (argc > 1)
     {
         task_file = fopen(argv[1], "r");
         //out_file = fopen(argv[2], "w");
     }
-    printf("S2");
+    //printf("S2");
     char *line = NULL;
     size_t len = 0;
     ssize_t read;
-    //tasks = malloc(sizeof(struct task));
+    tasks = malloc(sizeof(struct task));
     int x = 0;
-    printf("S3");
+    //printf("S3");
     while ((read = getline(&line, &len, task_file)) != -1)
     {
         x++;
-        //tasks = realloc(tasks, x * sizeof(struct task));
-        char *token = strtok(line, " ");
+        struct task *taskTmp;
+        taskTmp = malloc(sizeof(tasks));
+        taskTmp = tasks;
+        tasks = realloc(tasks, x * sizeof(struct task));
+        char *token = strtok(line, ":");
         int i = 0;
         // loop through the string to extract all other tokens
         while (token != NULL)
@@ -47,25 +97,36 @@ int main(int argc, char *argv[])
             switch (i)
             {
             case 0:
-                tasks[i].h = atoi(token);
+                tasks[x - 1].h = atoi(token);
                 break;
             case 1:
-                tasks[i].m = atoi(token);
+                tasks[x - 1].m = atoi(token);
                 break;
             case 2:
-                tasks[i].command = token;
+                tasks[x - 1].command = token;
                 break;
             case 3:
-                tasks[i].info = atoi(token);
+                tasks[x - 1].info = atoi(token);
                 break;
             }
-            token = strtok(NULL, " ");
+            token = strtok(NULL, ":");
             i++;
         }
     }
-    printf("S4");
-    for(int y=0;y<x;y++){
-        printf("Task nr: %d, h: %d, m: %d, Comm: %s, Inf: %d \n",y,tasks[y].h,tasks[y].m,tasks[y].command,tasks[y].info);
+    //printf("S4");
+    for (int h = x - 1; h >= 0; h--)
+    {
+        for (int y = 0; y < h; y++)
+        {
+            if ((tasks[y].h > tasks[y + 1].h) || (tasks[y].h == tasks[y + 1].h && tasks[y].m > tasks[y + 1].m))
+            {
+                swap(&tasks[y], &tasks[y + 1]);
+            }
+        }
+    }
+    for (int y = 0; y < x; y++)
+    {
+        printf("Task nr: %d, h: %d, m: %d, Comm: %s, Inf: %d \n", y, tasks[y].h, tasks[y].m, tasks[y].command, tasks[y].info);
     }
 
     pid_t pid, sid;
@@ -107,27 +168,33 @@ int main(int argc, char *argv[])
     close(STDIN_FILENO);
     close(STDOUT_FILENO);
     close(STDERR_FILENO);
-
+    //test();
     /* Daemon-specific initialization goes here */
-    time_t rawtime;
-    struct tm *info;
     /* The Big Loop */
-    while (1)
+    count = 0;
+    current_time = time(NULL);
+    info = localtime(&current_time);
+    info->tm_hour = tasks[count].h;
+    info->tm_min = tasks[count].m;
+    info->tm_sec = 0;
+    pid_t pidF;
+    pidF = fork();
+    if (pidF < 0)
     {
-        time(&rawtime);
-        info = localtime(&rawtime);
-        pid_t pidF;
-        pidF = fork();
-        if (pidF < 0)
-        {
-            exit(EXIT_FAILURE);
-        }
-        else if (pidF == 0)
-        {
-            execl("/usr/bin/firefox", NULL);
-        }
-        sleep(20);
+        exit(EXIT_FAILURE);
     }
+    else if (pidF == 0)
+    {
+        execl("/mnt/f/OS/Proj/Dem", NULL);
+    }
+    //alarm(difftime(timelocal(info), current_time));
+    //signal(SIGALRM, AlarmHandler);
+    /*while (1)
+    {
+        current_time = time(NULL);
+        info = localtime(&current_time);
+        sleep(60);
+    }*/
     closelog();
     exit(EXIT_SUCCESS);
 }
